ea8b19469c92352d3be7a6082326e6138a5407d7
fz6m <59400654+fz6m@users.noreply.github.com>
2024-05-05 11:03:55 +0900

    example: add demo

---
+++ examples/simple/.umirc.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "umi";
+
+export default defineConfig({
+  routes: [
+    { path: "/", component: "index" },
+    { path: "/docs", component: "docs" },
+  ],
+  npmClient: 'pnpm',
+  plugins: ['umi-plugin-ext-link-checker'],
+  linkChecker: {}
+});
---
+++ examples/simple/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Link } from 'umi'
+
+export default function HomePage() {
+  return (
+    <div>
+      <Link to="/docs">Go to docs</Link>
+    </div>
+  )
+}
